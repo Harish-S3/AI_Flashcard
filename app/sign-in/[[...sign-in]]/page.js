376d2ce773b74d8f19ec51299ede83a4ad7047b0
Harish <73985676+Harish-S3@users.noreply.github.com>
2024-08-30 08:04:38 +0530

    Update page.js

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,44 +1,23 @@
-import React from 'react';
-import { AppBar, Container, Toolbar, Typography, Button, Box, Paper } from "@mui/material";
-import Link from 'next/link';
-import { Book } from 'lucide-react';
-
-export default function SignUpPage() {
-  return (
+import {AppBar,Container,Toolbar,Typography,Button,Link,Box} from '@mui/material'
+import {SignIn} from '@clerk/nextjs'
+export default function SignUpPage(){
+  return(
     <Container maxWidth="sm">
-      <AppBar position="static" sx={{ backgroundColor: '#3f51b5', marginBottom: 4 }}>
+      <AppBar position="static" sx={{backgroundColor:'#3f51b5'}}>
         <Toolbar>
-          <Typography
-            variant="h6"
-            sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}
-          >
-            <Book size={24} style={{ marginRight: '0.5rem' }} />
-            Flashcard SaaS
-          </Typography>
-          <Button color="inherit" component={Link} href="/login">
-            Login
+          <Typography variant="h6"  sx={{ flexGrow: 1 }}>Flashcard SaaS</Typography>
+          <Button color="inherit">
+            <Link href="/login" passHref>Login</Link>
+          </Button>
+          <Button color="inherit">
+            <Link href="/signup" passHref>Sign Up</Link>
           </Button>
         </Toolbar>
       </AppBar>
-
-      <Paper elevation={3} sx={{ padding: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Sign Up
-        </Typography>
-        <Typography variant="body1" sx={{ mb: 3 }}>
-          Create your account to start making flashcards!
-        </Typography>
-        {/* Add your sign-up form here */}
-        <Box sx={{ width: '100%', mt: 2 }}>
-          {/* This is a placeholder for your actual sign-up form */}
-          <Button variant="contained" color="primary" fullWidth>
-            Sign Up with Email
-          </Button>
-        </Box>
-        <Typography variant="body2" sx={{ mt: 2 }}>
-          Already have an account? <Link href="/login">Log in</Link>
-        </Typography>
-      </Paper>
+      <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+        <Typography variant="h4">Sign In</Typography>
+        <SignIn/>
+      </Box>
     </Container>
-  );
+  )
 }
